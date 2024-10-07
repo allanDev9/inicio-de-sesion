fbd7ae9a6533cbaf3340e608e398e5fb74118003
allanDev9 <allanpenaguerra2003@gmail.com>
2024-10-06 23:01:38 -0600

    redireccionameinto de paginas y validaciones

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function login(){
+let intentosRestantes = 3;
 
-    let usuario = "Allan";
-    let contraseña = "123"
-    
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    let mensaje = document.getElementById('mensaje')
-    
-    if (username === usuario && password === contraseña){
-            mensaje.innerHTML = "Acceso exitoso ✅";
-    }
-    else{
-        mensaje.innerHTML = "Acceso denegado ❌";
-    }
-}
+        function login() {
+            let usuario = "Allan";
+            let contraseña = "123";
+            
+            let username = document.getElementById('username').value;
+            let password = document.getElementById('password').value;
+            let mensaje = document.getElementById('mensaje');
+            let boton = document.getElementById('button');
+
+            if (intentosRestantes > 0) {
+                if (username === usuario && password === contraseña) {
+                    mensaje.innerHTML = "Acceso exitoso ✅";
+                    setTimeout(() => {
+                        window.location.href="home.html"
+                    }, 2000);
+                } else {
+                    intentosRestantes--;
+                    if (intentosRestantes > 0) {
+                        mensaje.innerHTML = `Acceso denegado ❌. Te quedan ${intentosRestantes} intentos.`;
+                    } else {
+                        mensaje.innerHTML = "Has alcanzado el número máximo de intentos. Intenta de nuevo más tarde.";
+                        boton.disabled = true; 
+                        setTimeout(() => {
+                            boton.disabled = false; 
+                            intentosRestantes = 3;
+                        }, 3000); 
+                    }
+                }
+            } 
+        }
